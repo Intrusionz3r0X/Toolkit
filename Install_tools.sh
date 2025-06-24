c170c035271d364a2991c73e493e614a84fffb64
Intrusionz3r0X <165356542+Intrusionz3r0X@users.noreply.github.com>
2025-06-24 14:04:06 -0700

    Update Install_tools.sh
    add bloodhound.py and mingw-w64

--- Install_tools.sh
+++ Install_tools.sh
@@ -9,7 +9,7 @@ fi
 # Update & install tools via apt
 echo "[+] Updating & installing packages..."
 apt-get update -y
-apt-get install -y netexec seclists bloodhound certipy-ad bloodyad hoaxshell neo4j xxd moreutils feroxbuster enum4linux-ng coercer
+apt-get install -y netexec seclists bloodhound bloodhound.py certipy-ad bloodyad hoaxshell neo4j xxd moreutils feroxbuster enum4linux-ng coercer mingw-w64 
 
 # Decompress rockyou.txt
 echo "[+] Decompressing rockyou.txt..."
